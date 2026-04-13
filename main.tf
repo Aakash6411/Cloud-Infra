03e678e78fc393c03a87a5dd646dfa48ea84395d
Aakash6411 <as2042896@gmail.com>
2026-04-13 14:27:01 +0530

    Changes in main.tf file

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+provider "azurerm" {
+    subscription_id = "59949d18-ce39-4a44-a11f-9bb500a10229"
+    client_id = "0ff50672-1ca9-4aeb-b3ab-07c81305d40d"
+    client_secret = "ce0ab2bb-70ad-4c76-bc20-fd622330fc5d"
+    tenant_id = "1989444d-1ead-4896-a605-5083e777ddf9"
+    features {
+      
+    }
+}
+
 resource "azurerm_resource_group" "RG" {
   name     = "myrg"
   location = "West Europe"
