0b75035f7dbff82e79b3d75ac89d60cc71262f16
Aakash6411 <as2042896@gmail.com>
2026-04-14 12:54:48 +0530

    Vnet file

---
+++ vnet.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnetst1"
+  location            = "West Europe"
+  resource_group_name = "myrg"
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+  }
+  depends_on = [ azurerm_resource_group.RG ]
+
+  tags = {
+    name = "tftst"
+  }
+}
