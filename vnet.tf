d0aa0c74c0b8676d1114513adf2eabb6cd99cc59
Aakash6411 <as2042896@gmail.com>
2026-04-17 14:43:18 +0530

    Created a subnet as a seprate resource

--- vnet.tf
+++ vnet.tf
@@ -4,18 +4,23 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = local.resource_group_name
   address_space       = [local.virtual_network.address_space]
 
-  subnet {
-    name             = local.subnets[0].name
-    address_prefixes = local.subnets[0].address_prefixes
-  }
-
-  subnet {
-    name             = local.subnets[1].name
-    address_prefixes = local.subnets[1].address_prefixes
-  }
-  depends_on = [ azurerm_resource_group.RG ]
-
   tags = {
     name = "tftst"
   }
 }
+
+ resource "azurerm_subnet" "subnet1" {
+  name                 = local.subnet[0].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = local.subnets[0].address_prefixes
+  depends_on = [ local.virtual_network.vnetst1 ]
+  }
+
+  resource "azurerm_subnet" "subnet2" {
+  name                 = local.subnet[1].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = local.subnets[1].address_prefixes
+  depends_on = [ local.virtual_network.vnetst1 ]
+  }
