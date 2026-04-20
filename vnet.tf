d14b3e0287563f19dc61b0dbd73ec473494f23dd
Aakash6411 <as2042896@gmail.com>
2026-04-20 12:28:13 +0530

    Configured public ip

--- vnet.tf
+++ vnet.tf
@@ -25,3 +25,29 @@ resource "azurerm_virtual_network" "vnet1" {
   address_prefixes     = local.subnets[1].address_prefixes
   depends_on = [ azurerm_virtual_network.vnet1 ]
   }
+
+  resource "azurerm_network_interface" "nic" {
+  name = "nic1"
+  location = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal1"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.publicip1.id
+  }
+  depends_on = [ azurerm_subnet.subnetA ]
+}
+
+resource "azurerm_public_ip" "publicip1" {
+  name                = "test_publicip"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+  depends_on = [ azurerm_resource_group.RG ]
+}
